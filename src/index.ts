87c0f9bfc2ff01b314794158ce13a9131b1abd7a
Didas Mbalanya <didasmbalanya@gmail.com>
2020-10-05 17:47:41 +0200

    set up index.html node server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('well');
+import express from "express";
+
+// set up express server
+const app = express();
+
+
+// set up routes
+app.get("/users", (req, res) => {
+  res.status(200).send({ users: ["Didas"] });
+});
+
+// error handler for unknown routes
+app.use("*", (req, res) => {
+  res.status(404).send({ error: "route doesn't exist" });
+});
+
+// server port
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
