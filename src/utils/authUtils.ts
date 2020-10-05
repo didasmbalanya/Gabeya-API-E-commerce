a1ca28fe9c9adb42866793808d89fb9ca92fbe6f
Didas Mbalanya <didasmbalanya@gmail.com>
2020-10-05 18:52:52 +0200

    Set up auth utils

---
+++ src/utils/authUtils.ts
@@ -0,0 +1,44 @@
+import { sign, verify } from 'jsonwebtoken';
+import { hashSync, compareSync } from 'bcrypt';
+import { Request } from 'express';
+
+const JWT_SECRET = process.env.JWT_SECRET_ACCESS || 'secret';
+
+// bcrypt
+/**
+ * @function hashFunc set to 10 rounds of salt hashing rounds
+ * @param {string} password password to hash
+ * @returns {string} hashed value of the password input
+ */
+export const hashFunc = (password: string) => hashSync(password, 10);
+
+/**
+ *@function hashCompare used to compare plain text and hashed text
+ * @param {string} plainText
+ * @param {string} hashedText
+ * @returns {boolean}
+ */
+export const hashCompare = (plainText: string, hashedText: string) =>
+  compareSync(plainText, hashedText);
+
+/**
+ * @function jwtSignAccess function that manages signing access token 24hrs
+ * @param {string} userId passed in an object { userId } as payload, defaultValue: secret
+ */
+export const jwtSignAccess = (userId: number) => {
+  sign({ userId }, JWT_SECRET, { expiresIn: '24h' });
+};
+
+/**
+ * @function jwtVerify uses set secret to verify a token
+ * @param {string} token
+ */
+export const jwtVerify = (token: string) => verify(token, JWT_SECRET);
+
+/**
+ *@function getServerUrl Returns address of the running server
+ * @param {Request} req express request object
+ * @returns server url
+ */
+export const getServerUrl = (req: Request) =>
+  `${req.protocol}://${req.get('host')}/api/v1/user`;
---
+++ src/utils/constants.ts
@@ -0,0 +1,16 @@
+// a file that contains re usable constants
+
+// strings
+export const baseUrl = '/api/v1';
+
+// error messages
+export const noToken = 'token not provided';
+export const notFound = 'not found';
+export const notAuthenticated = 'not authenticated';
+export const notAllowed = 'not allowed';
+export const invalidToken = 'token content is invalid';
+export const invalidCredentials = 'invalid credentials';
+export const passwordErrorMessage =
+  'should have a minimum of five characters, at least one letter and one number';
+export const emailExistsError = 'user with the email already exists';
+// success strings
