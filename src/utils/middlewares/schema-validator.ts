f929bc9f0906e70237ff2fc900f31e1bc2d142a2
Didas Mbalanya <didasmbalanya@gmail.com>
2020-10-05 19:33:19 +0200

    add joi validator middleware

---
+++ src/utils/middlewares/schema-validator.ts
@@ -0,0 +1,26 @@
+import { Schema } from 'joi';
+import { RequestHandler, Request } from 'express';
+
+/**
+ *
+ * @param {*} schema validation schema
+ * @param {string} property specify which part of the request to validate
+ */
+export const validatorMiddleware = (
+  schema: Schema,
+  property: 'body' | 'params' | 'query'
+): RequestHandler => {
+  return (req, res, next) => {
+    const { error, value } = schema.validate(req[property], {
+      abortEarly: false,
+    });
+    const valid = error == null;
+    if (valid) {
+      req = value;
+      next();
+    } else {
+      // const { details } = error;
+      res.status(422).json({ error });
+    }
+  };
+};
