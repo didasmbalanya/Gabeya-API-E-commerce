82da3b51a466f5d87d3b09742d915fdeca2847d1
Didas Mbalanya <didasmbalanya@gmail.com>
2020-10-05 21:39:42 +0200

    fix request router handler

--- src/modules/cart/router.ts
+++ src/modules/cart/router.ts
@@ -1,4 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Dummy api for cart')
+})
+
 export default router;
--- src/modules/items/router.ts
+++ src/modules/items/router.ts
@@ -1,4 +1,8 @@
 import { Router } from 'express';
 const router = Router();
 
+router.get('/', (req, res) => {
+  res.send('Dummy api for items')
+})
+
 export default router;
